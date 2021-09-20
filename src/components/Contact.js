869cfe5f42f20c8e3a6df7fdd7aabe59a118c334
William <guillermoalvarez92@hotmail.com>
2021-09-20 10:46:30 +0100

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,33 @@
 import React from 'react'
+import phone from '../Images/phone.png'
+import mail from '../Images/email.png'
+import social from '../Images/user.png'
+import github from '../Images/github.png'
 
 const Contact = () => {
   return (
     <>
       <h1 className='text-center m-5'>Dont be shy and let&apos;s talk!</h1>
-      <div className='container boxes d-flex justify-content-between m-5'>
-        <div className='grid-container p-5 text-white bg-dark'>
-          <h1 className='grid-item text-center mb-5'>📞</h1>
-          <p>Hoooo</p>
-          <button>Here</button>
+      <div className='container boxes d-flex justify-content-between p-5'>
+        <div className='grid-container p-5 text-center text-white bg-dark'>
+          <img title='Made by https://www.freepik.com' src={phone} className='grid-item text-center mb-5'/>
+          <p className='text-center p-1'></p>
+          <a>07 459 526 933</a>
         </div>
-        <div className='grid-container p-5 text-white bg-dark'>
-          <h1 className='grid-item text-center mb-5'>📧</h1>
-          <p>Hoooo</p>
-          <button>Here</button>
+        <div className='grid-container text-center p-5 text-white bg-dark'>
+          <img title='Made by https://www.freepik.com' src={mail} className='grid-item text-center mb-5'/>
+          <p className='text-center'></p>
+          <a className="btn btn-secondary text-white" href="mailto: williamalvarez672@gmail.com" role="button">Email me</a>
         </div>
-        <div className='grid-container p-5 text-white bg-dark'>
-          <h1 className='grid-item text-center mb-5'>💼</h1>
-          <p>Hoooo</p>
-          <button>Here</button>
+        <div className='grid-container text-center p-5 text-white bg-dark'>
+          <img title='Made by https://www.freepik.com' src={social} className='grid-item text-center mb-5'/>
+          <p className='text-center'></p>
+          <a className="btn btn-secondary text-white" target='blank' rel="opener" href="https://www.linkedin.com/in/williamalvarez92/" role="button">Follow me</a>
+        </div>
+        <div className='grid-container text-center p-5 text-white bg-dark'>
+          <img title='Made by https://www.freepik.com' src={github} className='grid-item text-center mb-5'/>
+          <p className='text-center'></p>
+          <a className="btn btn-secondary text-white" target='blank' rel="opener" href="https://github.com/williamalvarez92" role="button">Github me</a>
         </div>
       </div>
     </>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/main.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
 
 ReactDOM.render(
