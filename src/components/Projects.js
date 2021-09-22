3d84e36b872b570b3e5ca6a98bd53f2b4f5bce71
William <guillermoalvarez92@hotmail.com>
2021-09-22 09:30:34 +0100

    updates

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Image } from 'react-bootstrap'
 import closed from '../Images/folder.png'
+import open from '../Images/folder_open.png'
 
 
 const Projects = () => {
@@ -7,15 +9,22 @@ const Projects = () => {
   
     <div className=' projectFolder container  d-flex justify-content-between p-5'>
       <div className='grid-container  m-5 text-center text-dark'>
-        <img title='Made by https://www.freepik.com' src={closed} className='grid-item text-center'/>
+        <Image className="social-icon mg" src = { closed }
+          onMouseOver={e => e.currentTarget.src = { open }}
+          onMouseOut={e => e.currentTarget.src = { closed }} 
+        />
         <p className='text-center'><a href=''>Personal Projects</a></p>
       </div>
       <div className='grid-container m-5 text-center text-dark'>
-        <img title='Made by https://www.freepik.com' src={closed} className='grid-item text-center'/>
+        <Image className="social-icon mg" src = { closed }
+          onMouseOver={e => e.currentTarget.src = { open }}
+          onMouseOut={e => e.currentTarget.src = { closed }} 
+        />
         <p className='text-center'><a href=''>General Assembly</a></p>
       </div>
       <div className='grid-container m-5 text-center text-dark'>
-        <img title='Made by https://www.freepik.com' src={closed} className='grid-item text-center'/>
+        <img title='Made by https://www.freepik.com' src={closed} className='grid-item img-btm text-center'/>
+        <img title='Made by https://www.freepik.com' src={open} className='grid-item img-top text-center'/>
         <p className='text-center'><a href=''>Small Projects</a></p>
       </div>
     </div>
