97b2123db44451a1bf096f1812f8af4e963444fd
William <guillermoalvarez92@hotmail.com>
2021-10-07 10:19:46 +0100

    all links fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,13 +9,13 @@ const Header = () => {
         <h1 className='m-2 navbar-brand text-white'>WA</h1>
       </div></Link>
       <div className='container navi justify-content-end'>
-        <Link to='/home'><div className=' m-3 navbar-item text-white'>
+        <Link to='/website/home'><div className=' m-3 navbar-item text-white'>
           Home
         </div></Link>
-        <Link to='/projects'><div className=' m-3 navbar-item text-white'>
+        <Link to='/website/projects'><div className=' m-3 navbar-item text-white'>
           Projects
         </div></Link>
-        <Link to='/contact'> <div className=' m-3 navbar-item text-white'>
+        <Link to='/website/contact'> <div className=' m-3 navbar-item text-white'>
           Contact me
         </div></Link>
       </div>
